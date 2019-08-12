171f9235927fb72157092368517a10d2ed2c9647
Richard Schneider <makaretu@gmail.com>
2019-08-12 12:00:53 +1200

    feat: a sample server

---
+++ NancyServer/HelloModule.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NancyServer
+{
+    public class HelloModule : NancyModule
+    {
+        public HelloModule()
+        {
+            Get("/", _ => "Hello World!");
+        }
+    }
+}
---
+++ NancyServer/Program.cs
@@ -0,0 +1,43 @@
+﻿using Makaretu.Nat;
+using Nancy.Hosting.Self;
+using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+
+namespace NancyServer
+{
+    class Program
+    {
+        static void Main()
+        {
+            var hostConfigs = new HostConfiguration
+            {
+                UrlReservations = new UrlReservations() { CreateAutomatically = true }
+            };
+            using (var nancyHost = new NancyHost(new Uri("http://localhost:8888")))
+            {
+                nancyHost.Start();
+                Console.WriteLine("listening on http://localhost:8888");
+
+                var endpoints = new List<LeasedEndpoint>();
+                foreach (var nat in NatDiscovery.GetNats())
+                {
+                    var lease = nat.CreatePublicEndpointAsync(ProtocolType.Tcp, 8888).Result;
+                    var endpoint = new LeasedEndpoint(lease);
+                    endpoints.Add(endpoint);
+                    Console.WriteLine($"listening on public address '{endpoint}'");
+                }
+
+                Console.WriteLine("Press enter to stop");
+                Console.ReadKey();
+
+                foreach (var endpoint in endpoints)
+                {
+                    endpoint.Dispose();
+                }
+            }
+
+            Console.WriteLine("Stopped. Good bye!");
+        }
+    }
+}
